81dd5f8b675db01db338ba28432b97896c2d4d46
Joon Lee <joon@act.md>
2016-11-01 09:17:49 -0400

    Initial commit

---
+++ lib/react/autocomplete/rails.rb
@@ -0,0 +1,9 @@
+require "react/autocomplete/rails/version"
+
+module React
+  module Autocomplete
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/react/autocomplete/rails/version.rb
@@ -0,0 +1,7 @@
+module React
+  module Autocomplete
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
